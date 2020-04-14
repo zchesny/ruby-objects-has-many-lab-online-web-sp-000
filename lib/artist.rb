c3b177dda276f2e41c1dcd3551933a5374e05ac3
Zoey Chesny <z.chesny@gmail.com>
2020-04-14 21:54:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,15 +4,19 @@ class Artist
 
   def initialize(name)
     @name = name
-
   end
 
   def songs
     Songs.all.select{|song| song.artist == artist}
   end
 
-  def add_song
+  def add_song(song)
+    song.artist = self
+  end
 
+  def add_song_by_name(name)
+    song = Song.new(name)
+    add_song(song)
   end
 
 
